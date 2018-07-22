f552fa88ec115dd31889c43234e08c8534d5e455
Ryota Ikezawa <pavegy@gmail.com>
2018-07-23 01:06:28 +0900

    Set up coverage report

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,22 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+
+# set up coverage directory
+SimpleCov.coverage_dir(File.join(ENV.fetch('CIRCLE_ARTIFACTS', 'public'), 'coverage'))
+SimpleCov.start do
+  add_filter '/spec/'
+  add_filter '/vendor/'
+end
+
+if ENV.fetch('CI', 'false') == 'true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
 require 'bundler/setup'
 require 'tinderb'
+require 'pry'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
